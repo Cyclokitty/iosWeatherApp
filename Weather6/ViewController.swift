9b52738912ecf748984fc928daed543ddf4bfb3a
user131652 <user131652@ny217.local>
2017-10-30 14:31:03 -0400

    Initial Commit

---
+++ Weather6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather6
+//
+//  Created by user131652 on 10/30/17.
+//  Copyright © 2017 user131652. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
